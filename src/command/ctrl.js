2e9e4c2488e53d665b2c2dbec88a784526613574
lkh4768 <lkh5510@gmail.com>
2019-07-16 00:28:16 +0900

    chore(lint): add eslint

--- src/command/ctrl.js
+++ src/command/ctrl.js
@@ -1,1 +1,3 @@
+exports.read = () => {};
 exports.runByKey = () => {};
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const cmdCtrl = require('./command/ctrl');
 const key = process.argv[1];
-const commands = {
-	pe: {
-		sudo: true,
-		command: 'ps -ef'
-	}
-};
 
-const main = async key => {
+const main = async (key) => {
 	await cmdCtrl.read();
 	const ret = await cmdCtrl.runByKey(key);
 	return ret;
