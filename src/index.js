05b9f4050a5d0bffff326ec3e7c26938a43c55fe
lkh4768 <lkh5510@gmail.com>
2019-07-21 23:24:41 +0900

    feat(main): add logging function and test code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const cmdCtrl = require('./command/ctrl');
 
 const main = key => {
-	const cmds = cmdCtrl.read();
-	const ret = cmdCtrl.runByKey(key, cmds);
-	return ret;
+  const cmds = cmdCtrl.read();
+  const cp = cmdCtrl.runByKey(key, cmds);
+  cmdCtrl.logging(cp);
 };
 
 const isDev = process.env.NODE_ENV === 'development';
 if (!isDev) {
-	main(process.argv[2]);
+  main(process.argv[2]);
 }
 
 exports.main = main;
