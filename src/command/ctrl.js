d43898649c6c03d9a68c0ea5e4ddcf552457f035
lkh4768 <lkh5510@gmail.com>
2019-07-21 10:30:02 +0900

    feat(command): add that parse to spawn args from command string

--- src/command/ctrl.js
+++ src/command/ctrl.js
@@ -7,3 +7,10 @@ exports.read = () => ({
 
 exports.runByKey = () => {};
 
+exports.parseCmdToArgsOfSpawn = cmd => {
+	if (!cmd || typeof cmd !== 'string') {
+		throw new Error('Invalid cmd type');
+	}
+
+	return cmd.split(' ');
+};
--- src/command/ctrl.test.js
+++ src/command/ctrl.test.js
@@ -1,4 +1,5 @@
 const cmdCtrl = require('./ctrl');
+const childProcess = require('child_process');
 
 describe('command/ctrl', () => {
 	describe('read', () => {
@@ -11,6 +12,52 @@ describe('command/ctrl', () => {
 					command: 'ps -ef'
 				}
 			});
+		});
+	});
+
+
+	describe('runByKey', () => {
+		const data = {
+			key: 'key',
+			cmd: 'ps',
+			args: ['-ef']
+		};
+		const rawCmd = `${data.cmd} ${data.args[0]}`;
+
+		test('Ensure that command seperated by cmd and args', () => {
+			const commands = {
+				[data.key]: { sudo: true, command: rawCmd }
+			};
+
+			childProcess.spawn = jest.fn().mockReturnValue({});
+
+			cmdCtrl.runByKey(data.key, commands);
+		});
+	});
+
+	describe('parseCmdToArgsOfSpawn', () => {
+		test('Throw error, when cmd is falsly', () => {
+			const falslyDataList = [ 0, false, null, undefined, '' ];
+
+			for(const falslyData of falslyDataList) {
+				expect(() => cmdCtrl.parseCmdToArgsOfSpawn(falslyData)).toThrow(Error);
+			}
+		});
+
+		test('Throw error, when cmd is not string', () => {
+			const isNotArrayList = [ [], {}, 123 ];
+
+			for(const isNotArray of isNotArrayList) {
+				expect(() => cmdCtrl.parseCmdToArgsOfSpawn(isNotArray)).toThrow(Error);
+			}
+		});
+
+		test('cmd split by space', () => {
+			const cmds = ['ps', '-ef'];
+			const cmd = cmds.join(' ');
+			const ret = cmdCtrl.parseCmdToArgsOfSpawn(cmd);
+
+			expect(ret).toEqual(cmds);
 		});
 	});
 });
