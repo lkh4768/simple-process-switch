c3b8d8aa210404f6943fd2f5c52a6c3848372fef
lkh4768 <lkh5510@gmail.com>
2019-07-23 00:09:27 +0900

    feat(config): add to get path of root

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,26 @@
-exports.getRootPath = () => {};
+const path = require('path');
+const {promises: fsPromises} = require('fs');
+
+exports.ROOT_PATH = path.join('~/', '.sps/');
+
+exports.getRootPath = async () => {
+  const isExistsRoot = await exports.existsRoot();
+  if (!isExistsRoot) {
+    await exports.makeRoot();
+  }
+  return exports.ROOT_PATH;
+};
+
+exports.existsRoot = async () => {
+  try {
+    await fsPromises.access(exports.ROOT_PATH);
+  } catch (err) {
+    console.log('Fail to access root', {error: err});
+    return false;
+  }
+  return true;
+};
+
+exports.makeRoot = async () => {
+  await fsPromises.mkdir(exports.ROOT_PATH, {recursive: true});
+};
--- src/config/index.test.js
+++ src/config/index.test.js
@@ -1,9 +1,75 @@
+const config = require('./index');
+const {promises: fsPromises} = require('fs');
+
 describe('config', () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   describe('getRootPath', () => {
-    test('get config path', () => {});
+    test('get root path of configuration', async () => {
+      const mockExistsRoot = jest
+        .spyOn(config, 'existsRoot')
+        .mockResolvedValue(true);
+      const mockMakeRoot = jest.spyOn(config, 'makeRoot').mockResolvedValue();
+
+      const rootPath = await config.getRootPath();
+
+      expect(mockMakeRoot).not.toHaveBeenCalled();
+      expect(mockExistsRoot).toHaveBeenCalledTimes(1);
+      expect(rootPath).toEqual(config.ROOT_PATH);
+    });
+
+    test('If root is not exists, it invokes function to make root.', async () => {
+      const mockExistsRoot = jest
+        .spyOn(config, 'existsRoot')
+        .mockResolvedValue(false);
+      const mockMakeRoot = jest.spyOn(config, 'makeRoot').mockResolvedValue();
+
+      const rootPath = await config.getRootPath();
+
+      expect(mockExistsRoot).toHaveBeenCalledTimes(1);
+      expect(mockMakeRoot).toHaveBeenCalledTimes(1);
+      expect(rootPath).toEqual(config.ROOT_PATH);
+    });
+  });
+
+  describe('existsRoot', () => {
+    test('If root is exists, it returns true', async () => {
+      const mockFsPromisesAccess = jest
+        .spyOn(fsPromises, 'access')
+        .mockResolvedValue();
+
+      const isExists = await config.existsRoot();
+
+      expect(mockFsPromisesAccess).toHaveBeenCalledTimes(1);
+      expect(isExists).toEqual(true);
+    });
+
+    test('If root is not exists, it returns false', async () => {
+      const mockFsPromisesAccess = jest
+        .spyOn(fsPromises, 'access')
+        .mockRejectedValue(new Error('TEST'));
+
+      const isExists = await config.existsRoot();
+
+      expect(mockFsPromisesAccess).toHaveBeenCalledTimes(1);
+      expect(isExists).toEqual(false);
+    });
+  });
+
+  describe('makeRoot', () => {
+    test('Make root', async () => {
+      const mockFsPromisesMkdir = jest
+        .spyOn(fsPromises, 'mkdir')
+        .mockImplementation((path, {recursive}) => {
+          expect(path).toEqual(config.ROOT_PATH);
+          expect(recursive).toEqual(true);
+        });
+
+      await config.makeRoot();
+
+      expect(mockFsPromisesMkdir).toHaveBeenCalledTimes(1);
+    });
   });
 });
