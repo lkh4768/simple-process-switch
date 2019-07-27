7e05dc15d441e26c336d4f3a62767a92f914a3de
lkh4768 <lkh5510@gmail.com>
2019-07-28 02:13:53 +0900

    feat(config): add to read config

--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,17 @@
 const sps = require('./index');
 const cmdCtrl = require('./command/ctrl');
+const config = require('./config');
 
 describe('main', () => {
   const key = 'key_1';
 
-  test('Run command by key', () => {
-    cmdCtrl.read = jest.fn().mockReturnValue({});
-    cmdCtrl.runByKey = jest.fn().mockReturnValue(true);
-    const ret = sps.main(key);
+  test('Run command by key', async () => {
+    config.read = jest.fn().mockResolvedValue({});
+    cmdCtrl.runByKey = jest.fn().mockResolvedValue();
 
-    expect(cmdCtrl.read).toHaveBeenCalledTimes(1);
+    const ret = await sps.main(key);
+
+    expect(config.read).toHaveBeenCalledTimes(1);
     expect(cmdCtrl.runByKey).toHaveBeenCalledTimes(1);
     expect(ret).toBeUndefined();
   });
