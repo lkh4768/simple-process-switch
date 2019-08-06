b934eef8d4e6b29ddb080d542346227d6b7e668b
lkh4768 <lkh5510@gmail.com>
2019-08-07 00:02:22 +0900

    feat(proc): add stoping process

--- src/proc/index.js
+++ src/proc/index.js
@@ -1,22 +1,52 @@
+const path = require('path');
 const childProcess = require('child_process');
 const {promises: fsPromises} = require('fs');
 const _ = require('lodash');
 
+const config = require('../config');
 const command = require('../command');
 const log = require('../log');
 
+exports.FILE_NAME = 'pid.json';
+
 exports.switchByKey = async (key, cmds) => {
-  if (exports.isRunning(key)) {
-    exports.killByKey(key);
+  const pid = await exports.getPid(key);
+  if (pid) {
+    exports.killByPid(pid);
+    await exports.delPid(key);
+    console.log(`Stop ${key}`);
     return;
   }
 
   const cmd = command.getByKey(key, cmds);
   await exports.runByKey(cmd, key);
+  console.log(`Start ${key}`);
 };
 
-exports.isRunning = () => {};
-exports.killByKey = () => {};
+exports.getPid = async key => {
+  try {
+    const pids = await exports.getPids();
+    return _.toInteger(pids[key]);
+  } catch (err) {
+    return null;
+  }
+};
+
+exports.getPids = async () => {
+  const pidFilePath = await exports.getPidFilePath();
+  const pidsStr = await fsPromises.readFile(pidFilePath, 'utf8');
+  return JSON.parse(pidsStr);
+};
+
+exports.killByPid = pid => {
+  console.log('killByPid', pid);
+  childProcess.exec(`kill -9 ${pid}`);
+};
+
+exports.delPid = async key => {
+  const pids = await exports.getPids();
+  await exports.savePids(_.omit(pids, [key]));
+};
 
 exports.runByKey = async (cmd, key) => {
   const argsOfSpawn = command.parseCmdToArgsOfSpawn(cmd);
@@ -24,20 +54,34 @@ exports.runByKey = async (cmd, key) => {
   const subProcess = childProcess.spawn(argsOfSpawn.cmd, argsOfSpawn.args, {
     stdio: [0, logFd, logFd],
     detached: true,
+    shell: true,
   });
-  await exports.savePid(subProcess.pid);
+  subProcess.unref();
+  await exports.addPid(key, subProcess.pid);
 };
 
-exports.savePid = async pid => {
-  if (!_.isInteger(pid)) {
+exports.addPid = async (key, pid) => {
+  if (!exports.validatePid(pid)) {
     throw new Error('The pid is not integer');
   }
-
-  // get pid file path
-  const pidFilePath = exports.getPidFilePath();
-
-  // append pid in pid file
-  await fsPromises.appendFile(pidFilePath, pid);
+  const pids = await exports.getPids();
+  await exports.savePids({...pids, [key]: pid});
 };
 
-exports.getPidFilePath = () => {};
+exports.validatePid = pid => _.isInteger(pid);
+
+exports.savePids = async pids => {
+  const pidFilePath = await exports.getPidFilePath();
+  await fsPromises.writeFile(pidFilePath, JSON.stringify(pids));
+};
+
+exports.getPidFilePath = async () => {
+  const configRootPath = await config.getRootPath();
+  const pidFilePath = path.join(configRootPath, exports.FILE_NAME);
+  try {
+    await fsPromises.access(pidFilePath);
+  } catch (err) {
+    await fsPromises.writeFile(pidFilePath, '');
+  }
+  return pidFilePath;
+};
--- src/proc/index.test.js
+++ src/proc/index.test.js
@@ -1,7 +1,9 @@
-const _ = require('lodash');
+const path = require('path');
 const childProcess = require('child_process');
 const {promises: fsPromises} = require('fs');
+const _ = require('lodash');
 
+const config = require('../config');
 const proc = require('./index');
 const command = require('../command');
 const log = require('../log');
@@ -13,6 +15,9 @@ describe('proc', () => {
     args: ['-ef'],
   };
   const rawCmd = `${data.cmd} ${data.args[0]}`;
+  const commands = {
+    [data.key]: rawCmd,
+  };
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -20,41 +25,107 @@ describe('proc', () => {
 
   describe('switchByKey', () => {
     test('If process is not running, process started by key', async () => {
-      const commands = {
-        [data.key]: rawCmd,
-      };
-
-      const mockIsRunning = jest
-        .spyOn(proc, 'isRunning')
-        .mockReturnValue(false);
-      const mockKillByKey = jest.spyOn(proc, 'killByKey').mockReturnValue();
+      const mockGetPid = jest.spyOn(proc, 'getPid').mockResolvedValue(false);
+      const mockKillByPid = jest.spyOn(proc, 'killByPid').mockReturnValue();
+      const mockDelPid = jest.spyOn(proc, 'delPid').mockReturnValue();
       const mockRunByKey = jest.spyOn(proc, 'runByKey').mockResolvedValue();
       command.getByKey = jest.fn().mockReturnValue();
 
       await proc.switchByKey(data.key, commands);
 
-      expect(mockIsRunning).toHaveBeenCalledTimes(1);
+      expect(mockGetPid).toHaveBeenCalledTimes(1);
       expect(mockRunByKey).toHaveBeenCalledTimes(1);
-      expect(mockKillByKey).not.toHaveBeenCalled();
+      expect(mockKillByPid).not.toHaveBeenCalled();
+      expect(mockDelPid).not.toHaveBeenCalled();
       expect(command.getByKey).toHaveBeenCalledTimes(1);
     });
 
     test('If process is running, process killed by key', async () => {
-      const commands = {
-        [data.key]: rawCmd,
-      };
-
-      const mockIsRunning = jest.spyOn(proc, 'isRunning').mockReturnValue(true);
-      const mockKillByKey = jest.spyOn(proc, 'killByKey').mockReturnValue();
+      const mockGetPid = jest.spyOn(proc, 'getPid').mockResolvedValue(true);
+      const mockKillByPid = jest.spyOn(proc, 'killByPid').mockReturnValue();
+      const mockDelPid = jest.spyOn(proc, 'delPid').mockReturnValue();
       const mockRunByKey = jest.spyOn(proc, 'runByKey').mockResolvedValue();
       command.getByKey = jest.fn().mockReturnValue();
 
       await proc.switchByKey(data.key, commands);
 
-      expect(mockIsRunning).toHaveBeenCalledTimes(1);
+      expect(mockGetPid).toHaveBeenCalledTimes(1);
       expect(mockRunByKey).not.toHaveBeenCalled();
       expect(command.getByKey).not.toHaveBeenCalled();
-      expect(mockKillByKey).toHaveBeenCalledTimes(1);
+      expect(mockKillByPid).toHaveBeenCalledTimes(1);
+      expect(mockDelPid).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getPid', () => {
+    test('If written pid in pid file, return pid', async () => {
+      const pids = {
+        [data.key]: 1,
+      };
+
+      const mockGetPids = jest.spyOn(proc, 'getPids').mockResolvedValue(pids);
+
+      const getPid = await proc.getPid(data.key);
+
+      expect(getPid).toEqual(1);
+      expect(mockGetPids).toHaveBeenCalledTimes(1);
+    });
+
+    test('When any function in the getPid is throwing error, return null', async () => {
+      const pids = {
+        [data.key]: 1,
+      };
+
+      const mockGetPids = jest.spyOn(proc, 'getPids').mockRejectedValue(pids);
+
+      const getPid = await proc.getPid(data.key);
+
+      expect(getPid).toEqual(null);
+      expect(mockGetPids).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getPids', () => {
+    test('get pid list', async () => {
+      const expectPids = {
+        [data.key]: 1,
+      };
+
+      const mockGetPidFilePath = jest
+        .spyOn(proc, 'getPidFilePath')
+        .mockResolvedValue('path');
+      const mockReadFile = jest
+        .spyOn(fsPromises, 'readFile')
+        .mockResolvedValue(JSON.stringify(expectPids));
+      const mockAppendFile = jest
+        .spyOn(fsPromises, 'writeFile')
+        .mockResolvedValue();
+
+      const pids = await proc.getPids();
+
+      expect(pids).toEqual(expectPids);
+    });
+  });
+
+  describe('killByPid', () => {
+    test('Kill process by pid', () => {
+      const mockExec = jest.spyOn(childProcess, 'exec');
+
+      proc.killByPid();
+
+      expect(mockExec).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('delPid', () => {
+    test('del pid by key in pid file', async () => {
+      const mockGetPids = jest.spyOn(proc, 'getPids').mockResolvedValue({});
+      const mockSavePids = jest.spyOn(proc, 'savePids').mockResolvedValue();
+
+      await proc.delPid(data.key, 1);
+
+      expect(mockGetPids).toHaveBeenCalledTimes(1);
+      expect(mockSavePids).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -62,62 +133,111 @@ describe('proc', () => {
     const rawCmd = `${data.cmd} ${data.args[0]}`;
 
     test('Run command', async () => {
-      const commands = {
-        [data.key]: rawCmd,
-      };
-      const expectedRet = {};
+      const expectedRet = {unref: () => {}};
 
       command.parseCmdToArgsOfSpawn = jest
         .fn()
-        .mockReturnValue(_.pick(data, ['proc', 'args']));
+        .mockReturnValue({cmd: '', args: ''});
       log.openLogFileUsingKey = jest.fn().mockResolvedValue(`./${data.key}`);
       const mockSpawn = jest
         .spyOn(childProcess, 'spawn')
         .mockReturnValue(expectedRet);
-      const mockSavePid = jest.spyOn(proc, 'savePid').mockResolvedValue();
+      const mockAddPid = jest.spyOn(proc, 'addPid').mockResolvedValue();
 
       await proc.runByKey(data.key, commands);
 
       expect(command.parseCmdToArgsOfSpawn).toHaveBeenCalledTimes(1);
       expect(log.openLogFileUsingKey).toHaveBeenCalledTimes(1);
       expect(mockSpawn).toHaveBeenCalledTimes(1);
-      expect(mockSavePid).toHaveBeenCalledTimes(1);
+      expect(mockAddPid).toHaveBeenCalledTimes(1);
     });
   });
 
-  describe('savePid', () => {
-    const notIntList = [
-      '',
-      'str',
-      new Date(),
-      [],
-      true,
-      false,
-      () => {},
-      null,
-      undefined,
-    ];
-
-    test('If pid is not integer, throw error', () => {
-      for (const notInt of notIntList) {
-        expect(proc.savePid(notInt)).rejects.toThrow(Error);
-      }
+  describe('addPid', () => {
+    test('If pid is not valid, throw error', () => {
+      const mockGetPids = jest
+        .spyOn(proc, 'validatePid')
+        .mockReturnValue(false);
+      expect(proc.addPid()).rejects.toThrow(Error);
     });
 
-    test('Write pid in pid file', async () => {
-      const pid = 1;
+    test('add pid', async () => {
+      const mockGetPids = jest.spyOn(proc, 'getPids').mockResolvedValue({});
+      const mockSavePids = jest.spyOn(proc, 'savePids').mockResolvedValue();
+
+      await proc.addPid(data.key, 1);
+
+      expect(mockGetPids).toHaveBeenCalledTimes(1);
+      expect(mockSavePids).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('savePids', () => {
+    test('Write pids in pid file', async () => {
+      const pids = {[data.key]: 1};
+      const filePath = 'path';
 
       const mockGetPidFilePath = jest
         .spyOn(proc, 'getPidFilePath')
-        .mockReturnValue('path');
-      const mockAppendFile = jest
-        .spyOn(fsPromises, 'appendFile')
-        .mockResolvedValue();
+        .mockResolvedValue(filePath);
+      const mockWriteFile = jest
+        .spyOn(fsPromises, 'writeFile')
+        .mockImplementation((f, str) => {
+          expect(f).toEqual(filePath);
+          expect(str).toEqual(JSON.stringify(pids));
+          return Promise.resolve();
+        });
 
-      await proc.savePid(pid);
+      await proc.savePids(pids);
 
       expect(mockGetPidFilePath).toHaveBeenCalledTimes(1);
-      expect(mockAppendFile).toHaveBeenCalledTimes(1);
+      expect(mockWriteFile).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getPidFilePath', () => {
+    const configPath = path.resolve('./');
+    const pidFilePath = path.join(configPath, proc.FILE_NAME);
+    const makeMockWriteFile = () =>
+      jest.spyOn(fsPromises, 'writeFile').mockImplementation(filepath => {
+        expect(filepath).toEqual(pidFilePath);
+        return Promise.resolve();
+      });
+    const makeMockGetRootPath = () => jest.fn().mockResolvedValue(configPath);
+    const makeMockAccess = ret =>
+      jest.spyOn(fsPromises, 'access').mockImplementation(filepath => {
+        expect(filepath).toEqual(pidFilePath);
+        return ret;
+      });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    test('Get a file path of pid list', async () => {
+      config.getRootPath = makeMockGetRootPath();
+      const mockAccess = makeMockAccess(Promise.resolve(true));
+      const mockWriteFile = makeMockWriteFile();
+
+      const ret = await proc.getPidFilePath();
+
+      expect(ret).toEqual(pidFilePath);
+      expect(config.getRootPath).toHaveBeenCalledTimes(1);
+      expect(mockWriteFile).not.toHaveBeenCalled();
+      expect(mockAccess).toHaveBeenCalledTimes(1);
+    });
+
+    test('If not found pid file, make pid file and get a file path of pid list', async () => {
+      config.getRootPath = makeMockGetRootPath();
+      const mockAccess = makeMockAccess(Promise.reject(new Error()));
+      const mockWriteFile = makeMockWriteFile();
+
+      const ret = await proc.getPidFilePath();
+
+      expect(ret).toEqual(pidFilePath);
+      expect(config.getRootPath).toHaveBeenCalledTimes(1);
+      expect(mockWriteFile).toHaveBeenCalledTimes(1);
+      expect(mockAccess).toHaveBeenCalledTimes(1);
     });
   });
 });
